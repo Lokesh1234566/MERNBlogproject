88d48bedc4c34e4b34f0b540b1ffae740b62f153
Lokesh R <lokesh21031999@gmail.com>
2024-02-02 15:29:24 +0530

    noused

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Project from "./Pages/Project";
-import Dashboard from "./Pages/Dashboard";
-import SignIn from "./Pages/SignIn";
-import SignUp from "./Pages/SignUp";
 import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import PrivateRoute from "./Components/PrivateRoute";
-import OnlyAdminPrivateRoute from "./Components/OnlyAdminPrivateRoute";
-import CreatePost from "./Pages/CreatePost";
-import UpdatePost from "./Pages/UpdatePost";
-import PostPage from "./Pages/PostPage";
-import ScrollToTop from "./Components/ScrollToTop";
-import Search from "./Pages/Search";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/search" element={<Search />} />
-
-          <Route element={<PrivateRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-          <Route element={<OnlyAdminPrivateRoute />}>
-            <Route path="/create-post" element={<CreatePost />} />
-            <Route path="/update-post/:postId" element={<UpdatePost />} />
-          </Route>
-
-          <Route path="/projects" element={<Project />} />
-          <Route path="/post/:postSlug" element={<PostPage />} />
-        </Routes>
-
-        <Footer />
-      </BrowserRouter>
+      <Header />
     </>
   );
 };
